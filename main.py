7c397485004858641b0009d58d90e73cbee0ca92
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-09-20 13:45:00 -0300

    Documentação

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ class Conversor:
         quociente = []
         while True:
             # Coloca o Resto e o Quociente nas listas correspondentes.
-            quociente.append(int(decimal / 2))
+            quociente.append(decimal // 2)
             resto.append(int(decimal % 2))
-            decimal = decimal / 2
+            decimal = decimal // 2
             # Pega o último valor do Quociente e coloca na lista de Restos.
             if decimal < 2:
                 ultimo = len(quociente) - 1
@@ -29,26 +29,23 @@ class Conversor:
                 break
         # Inverte a lista de trás para frente.
         resto = resto[::-1]
+        # Retorna o valor Binário.
         print('\nBinário:', end=' ')
         for c in resto:
             print(c, end='')
 
     @staticmethod
     def Decimal_Hexadecimal(decimal):
-        tabela_hex = {'A': 10,
-                      'B': 11,
-                      'C': 12,
-                      'D': 13,
-                      'E': 14,
-                      'F': 15}
+        tabela_hex = {10: 'A', 11: 'B', 12: 'C',
+                      13: 'D', 14: 'E', 15: 'F'}
         # Cria listas vazias.
         resto = []
         quociente = []
         while True:
             # Coloca o Resto e o Quociente nas listas correspondentes.
-            quociente.append(int(decimal / 16))
+            quociente.append(decimal // 16)
             resto.append(int(decimal % 16))
-            decimal = decimal / 16
+            decimal = decimal // 16
             # Pega o último valor do Quociente e coloca na lista de Restos.
             if decimal < 16:
                 ultimo = len(quociente) - 1
@@ -56,8 +53,15 @@ class Conversor:
                 break
         # Inverte a lista de trás para frente.
         resto = resto[::-1]
-        print('\nHexadecimal:')
-        print(resto)
+        # Converte os valores da tabela em Letras Hexadecimais.
+        for v in resto:
+            if v in tabela_hex.keys():          # Se 'v' for uma chave do dicionário.
+                i = resto.index(v)              # Pega o index com o valor correspondente a chave.
+                resto[i] = tabela_hex[v]        # Substitui pelo valor da chave.
+        # Retorna o valor Hexadecimal.
+        print('\nHexadecimal:', end=' ')
+        for c in resto:
+            print(c, end='')
 
 
 usuario = Conversor()
